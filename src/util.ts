a983574d9a460816b9f8ceb25d89c8870812d73e
Georgiy Komarov <jubnzv@gmail.com>
2024-10-09 09:11:43 +0000

    chore: Filter-out emojis from the code base

---
+++ src/util.ts
@@ -0,0 +1,9 @@
+/**
+ * Helper symbols to generate distracting colorful output for gen-Z users
+ */
+export class Sym {
+  public static OK = "✅";
+  public static WARN = "⚠️";
+  public static ERR = "❌";
+  public static WAIT = "⏳";
+}
